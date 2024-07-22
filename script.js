ab6dffeab1a69db0f838b6cbda495c31dfe83712
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-22 18:47:39 +0200

    Working with data type, PRIMITIVE DATA TYPE

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let js = "amazing";
-if (js === "amazing") alert("Javascript is amazing");
+if (js === "amazing") console.log("Javascript is amazing");
 console.log(4 + 6 + 10);
 
 let job1 = "programmer";
@@ -16,3 +16,19 @@ let lastName = "Guebre";
 // let new='chair'; NOT ALLOWED
 
 const PI = 3.141516;
+
+// PRIMITIVE DATA TYPE
+let job = "Adama";
+let age = 28;
+let isMaried = true;
+
+let children;
+23;
+console.log(typeof children);
+console.log(typeof true);
+console.log(typeof "Value");
+
+isMaried = 1;
+console.log(isMaried);
+isMaried = false;
+console.log(typeof isMaried);
