e0cde91ddf13e960c167a41f8db906b6940c4615
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-22 18:22:25 +0200

    learning variables fundamentals

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let js = "amazing";
 if (js === "amazing") alert("Javascript is amazing");
 console.log(4 + 6 + 10);
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+let firstJob = "programmer";
+let currentJob = "teacher";
+
+let firstName = "Adam";
+let lastName = "Guebre";
+
+// let 1Name='Adama' NOT ALLOWED
+// let firstName&lastName='Guebre Adama' NOT ALLOWED
+// let new='chair'; NOT ALLOWED
+
+const PI = 3.141516;
