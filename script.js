f9f60069f2d977d359789257f8d82c0f773ddb9b
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-22 19:49:01 +0200

    Working with operators

--- script.js
+++ script.js
@@ -36,3 +36,25 @@ console.log(typeof isMaried);
 const birthDay = "Monday";
 // birthDay = "Tuesday"; NOT ALLOWED
 // const birthYear; NOT ALLOWED
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+console.log((ageSarah * 2) / ageJonas);
+
+console.log(ageJonas ** 4);
+let x = 10 + 5;
+x += 10;
+console.log(x);
+x *= 10;
+x -= 7;
+x /= 2;
+x++;
+x--;
+console.log(x);
+console.log(firstName + " " + lastName);
+
+console.log(ageJonas <= ageSarah);
+console.log(ageSarah >= 18);
+console.log(ageSarah === ageJonas);
+const isFullAge = ageSarah >= 18;
