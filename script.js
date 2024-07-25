4fa638a03074d5913d2b31b9494a81a0acc4d187
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-25 19:17:15 +0200

    Working with arrow function

--- script.js
+++ script.js
@@ -256,3 +256,19 @@ const calcage2 = function (birthYear) {
 };
 const age2 = calcage2(1991);
 console.log(age2);
+
+// ARROW FUNCTION
+
+const calcAge3 = (birthYear) => 2037 - birthYear;
+
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearUntilRetirement(1991, "Adam"));
+console.log(yearUntilRetirement(2001, "Bob"));
