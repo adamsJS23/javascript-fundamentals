930b67c7b6af214efa5964442c3f5a9df769f0aa
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-27 17:37:04 +0200

    Challenge #2.3 completed

--- script.js
+++ script.js
@@ -485,3 +485,39 @@ adamObject.getSummary = function () {
 };
 console.log(adamObject);
 adamObject.getSummary();
+
+// CHALLENGE #2.3
+
+console.log("CHALLENGE #2.3");
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+console.log(mark, john);
+mark.calcBMI();
+john.calcBMI();
+if (mark.bmi > john.bmi)
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+  );
+else
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
+  );
