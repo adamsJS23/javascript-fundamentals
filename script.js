903aa2d6efe7e5cd2949eb095b25e59da1bd707b
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-27 15:40:11 +0200

    Challenge #2.2 completed

--- script.js
+++ script.js
@@ -386,3 +386,29 @@ else console.log("Invalid year");
 years.includes(1999)
   ? console.log("You choose the right year")
   : console.log("Invalid year");
+
+// CHALLENGE #2.2
+console.log("CHALLENGE #2.2");
+
+const bill = 100;
+
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(calcTip(100));
+
+const bills = new Array(125, 555, 44);
+
+// TEST DATA: 125, 555, and 44.
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+console.log("Tips array", tips);
+
+const totals = new Array(
+  bills[0] + tips[0],
+  bills[1] + tips[1],
+  bills[2] + tips[2]
+);
+
+console.log("Bill + tip array", totals);
